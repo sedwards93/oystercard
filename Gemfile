65d2f35740d521a6f77c1493bdac04141f199916
Symion Edwards <symion.edwards@gmail.com>
2020-11-02 15:48:30 +0000

    Initialized with balance of 0

--- Gemfile
+++ Gemfile
@@ -1,10 +1,9 @@
 source "https://rubygems.org"
 
-git_source(:github) {|oystercard| "https://github.com/sedwards93/oystercard"}
+# git_source(:github) {|oystercard| "https://github.com/sedwards93/oystercard"}
 
 ruby '2.7.1'
 
 group :development, :test do
   gem "rspec"
 end
-
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,9 @@
 require 'oystercard'
 describe Oystercard do
+  it "Can create instances of the Oystercard class" do
+    expect(subject).to be_an_instance_of(Oystercard)
+  end
+  it "Instances of the Oystercard class are initialized witha balance of 0" do
+    expect(subject.balance).to eq(0)
+  end
 end
